5e1cc453ca4138c1076127ac3c846dc612167eef
iArcadia <bibollet.kevin@gmail.com>
2025-04-16 15:49:04 +0200

    separated bootstrap js files

--- www/resources/js/app.js
+++ www/resources/js/app.js
@@ -1,1 +1,2 @@
-import './bootstrap';
+import './boostrap/axios.js';
+import './boostrap/inertia.js';
--- www/resources/js/boostrap/inertia.js
+++ www/resources/js/boostrap/inertia.js
@@ -1,21 +1,17 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
-import App from './Pages/App.vue';
-import axios from 'axios';
-
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import App from './../Pages/App.vue';
 
 createInertiaApp({
     resolve: name => {
         const
-            pages = import.meta.glob('./Pages/**/*.vue', {eager: true}),
-            page = pages[`./Pages/${name}.vue`]
+            pages = import.meta.glob('./../Pages/**/*.vue', {eager: true}),
+            page = pages[`../Pages/${name}.vue`]
         ;
 
         page.default.layout = page.default.layout || App
 
-        return pages[`./Pages/${name}.vue`];
+        return page;
     },
 
     setup({el, App, props, plugin}) {
